55ab1d4049d36f3a2be813d3ce2b647e2185d527
avance2 <35709266+avance2@users.noreply.github.com>
2018-03-12 22:07:23 -0700

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+console.log('Be like Browser.');
+
+// $('video').tubular({videoId : 'sw5S2-T-Ogk',
+// });
+
+// Light Gallery
+$('#gallery-1').lightGallery();
+
+// $('.para').imageScroll({
+	
+// });
+
+$('#example-3').elevateZoom({
+  zoomType : "lens",
+  lensShape : "round",
+  lensSize : 200
+});
+
